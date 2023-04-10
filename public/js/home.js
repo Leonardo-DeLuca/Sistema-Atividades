7a64fc126156787acb99fc8b01c2b7fcd6a4053f
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-04-10 10:53:07 -0300

    inicio paginas result e testes

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,13 @@
+const URL_BASE = window.location.href.substring(0, 21);
+
 const onClickLogin = async () => {
-    window.location.href = "http://localhost:3000/login";
+    window.location.href = `${URL_BASE}/login`;
+};
+
+const onClickResultados = async () => {
+    window.location.href = `${URL_BASE}/resultado`;
+};
+
+const onClickTestes = async () => {
+    window.location.href = `${URL_BASE}/testes`;
 };
