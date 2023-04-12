9ae0b93f18de4b8cfb6c50ce0e87d90490ae1e9d
Leonardo-DeLuca <leonardo.s.deluca@gmail.com>
2023-04-12 18:41:15 -0300

    Lista testes concluida

--- public/js/lista_testes.js
+++ public/js/lista_testes.js
@@ -16,22 +16,43 @@ const carregaItemsLista = async () => {
     let obj = {}
 
     data.forEach(element =>{
-        obj = {id: element.id}
+        console.log(element)
+        adicionaItemsLista(element)
     });
 
     console.log(obj)
 
 }
 
-const adicionaItemsLista = async (data) =>{
+const adicionaItemsLista = async (element) =>{
 
     const tabela = document.getElementById("tabelaTestes")
     const listItem = document.createElement("li")
     listItem.classList = "table-row"
-    const div1 = document.createElement("div")
-    div1.classList = "col col-1"
-    div1.innerHTML = 'id'
 
+    for(let i = 1; i <= 5; i++){
+        window['div'+i] = document.createElement('div')
+    }
+
+    // const [div1, div2, div3, div4, div5] = document.createElement("div")
+    div1.classList = "col col-1"
+    div1.innerHTML = element.id
+
+    div2.classList = "col col-2"
+    div2.innerHTML = element.nome
+
+    div3.classList = "col col-3"
+    div3.innerHTML = element.materia
+
+    div4.classList = "col col-4"
+    div4.innerHTML = element.dificuldade
+
+    div5.classList = "col col-5"
+    div5.innerHTML = element.perguntas.length
+    
     tabela.appendChild(listItem)
-    listItem.appendChild(div1)
+    for(let i = 1; i <= 5; i++){
+        listItem.appendChild( window['div'+i])
+    }
+    
 }
